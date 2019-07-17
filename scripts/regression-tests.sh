ee95fa36d9fb53c2d6ca8df3ee8f8301aba41047
Valerie R Young <valerie@bocoup.com>
2019-07-17 15:14:26 -0400

    Actually add script

--- scripts/regression-tests.sh
+++ scripts/regression-tests.sh
@@ -6,7 +6,26 @@ then
   exit
 fi
 
-TEST_FILES=$(git diff --name-only | grep -oP 'test/tests/\K\w+(?=.js)')
-EXAMPLE_FILES=$(git diff --name-only | grep -oP 'examples/\K\w+(?=/)' | uniq)
+TEST_INFRA=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -oP 'test/(util|index)')
+TEST_FILES=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -oP 'test/tests/\K\w+(?=.js)')
+EXAMPLE_DIRS=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -oP 'examples/\K\w+(?=/)' | uniq)
 
-# Run ava with multiple: --match "*TEST_FILES*"
+ARGS=''
+
+# Only add match args if the example/js or example/css directories or test/index.hs
+# or the test/utils.js directories have not been edited
+
+if [ ! $TEST_INFRA ] || [ ! echo "$EXAMPLE_DIRS" | grep --silent -P "^(js|css)$" ]
+then
+  for D in $EXAMPLE_DIRS
+  do
+    ARGS="${ARGS} --match *${D}*"
+  done
+
+  for F in $TEST_FILES
+  do
+    ARGS="${ARGS} --match *${F}*"
+  done
+fi
+
+ava -t -c 1 test/tests "ARGS"
